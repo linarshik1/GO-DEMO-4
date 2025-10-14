73ba3b4d86801f2f44ae2449f8af7e09a0deaf1d
linarshik1 <linarmakh20.06@gmail.com>
2025-10-14 19:54:20 +0300

    feat: добавлен менеджер паролей с CRUD-операциями

--- main.go
+++ main.go
@@ -7,31 +7,64 @@ import (
 )
 
 func main() {
+	// 1 Создать аккаунт
+	// 2 Найти аккаунт
+	// 3 Удалить аккаунт
+	// 4 Выход
+	fmt.Println("___Менеджер паролей___")
+Menu:
+	for {
+		variant := getMenu()
+		switch variant {
+		case 1:
+			createAccount()
+		case 2:
+			findAccount()
+		case 3:
+			deleteAccount()
+		default:
+			break Menu
+		}
+	}
 	createAccount()
 }
 
-func createAccount() {
-	login := promptData("Введите логин:")
-	password := promptData("Введите пароль:")
-	url := promptData("Введите URL:")
+func getMenu() int {
+	var variant int
+	fmt.Println("Выберите вариант:")
+	fmt.Println("1 Создать аккаунт")
+	fmt.Println("2 Найти аккаунт")
+	fmt.Println("3 Удалить аккаунт")
+	fmt.Println("4 Выход")
+	fmt.Scan(&variant)
+	return variant
+}
 
+func findAccount() {
+
+}
+
+func deleteAccount() {
+
+}
+
+func createAccount() {
+	login := promptData("Введите логин")
+	password := promptData("Введите пароль")
+	url := promptData("Введите  URL")
 	myAccount, err := account.NewAccount(login, password, url)
 	if err != nil {
 		fmt.Println("Неверный формат URL или Логин")
 		return
 	}
-	file, err := myAccount.ToBytes()
+	vault := account.NewVault()
+	vault.AddAccount(*myAccount)
+	data, err := vault.ToBytes()
 	if err != nil {
 		fmt.Println("Не удалось преобразовать в JSON")
 		return
 	}
-
-	err = files.WriteFile(file, "data.json")
-	if err != nil {
-		fmt.Println("Не удалось записать файл:")
-		return
-	}
-	fmt.Println("Аккаунт сохранен в data.json")
+	files.WriteFile(data, "data.json")
 }
 
 func promptData(prompt string) string {
